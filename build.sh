6b1bcad8503b0b1cef228671d35a7813ddcb573e
Charlie <charlie@i-am.cool>
2024-07-10 12:59:23 +0100

    enable NX/full RELRO

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 ./generate_mime_tables.py
 
 nasm -felf64 -g webserver.asm
-ld -dynamic-linker /lib64/ld-linux-x86-64.so.2 -o webserver -lc webserver.o
+ld -dynamic-linker /lib64/ld-linux-x86-64.so.2 -o webserver -lc webserver.o -z noexecstack -z relro -z now
